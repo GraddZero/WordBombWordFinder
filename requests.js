5785a86d52044199098e8e66dd95dcc6cc2b960b
GraddZero <75500295+GraddZero@users.noreply.github.com>
2021-08-22 13:49:12 +0100

    Update requests.js

--- requests.js
+++ requests.js
@@ -1,25 +1,39 @@
 const sendRequest = (method, url, data) => {
- var xhr = new XMLHttpRequest;
-	
- xhr.responseType = 'json';
- if (data) {data = JSON.stringify(data)}}
-	
-	
-
-
-			getWords = () => {
-				console.log("getting words")
-				const xhr = new XMLHttpRequest();
-				const url = "https://api.datamuse.com/words?sp=";
-				xhr.responseType = 'json';
-				
-				const query = document.getElementById("input").value;
-				const endpoint = url + '*' + query + '*';
-				xhr.onreadystatechange = () => {
-					if (xhr.readyState === XMLHttpRequest.DONE) {
-						renderResponse(xhr.response);
-					}
-				}
-				xhr.open('GET',endpoint)
-				xhr.send()
+	const promise = new Promise((resolve, reject) => {
+		const xhr = new XMLHttpRequest();
+		xhr.open(method, url);
+		
+		xhr.responseType = 'json';
+		
+		if (data) {
+			xhr.setRequestHeader('Content-Type','application/json');
+		}
+		
+		xhr.onreadystatechange = () => {
+			if(xhr.readyState === XMLHttpRequest.DONE) {
+				resolve(xhr.response);
+			} else {
+				reject(xhr.response);
 			}
+		}
+		xhr.onerror = () => {
+			reject('uhhh... idek');
+		}
+		
+		xhr.send(JSON.stringify(data));
+	});
+	return promise;
+};
+
+getWords = () => {
+	console.log("getting words")		
+	const query = document.getElementById("input").value;
+	const endpoint = 'https://api.datamuse.com/words?sp=*' + query + '*';
+	sendRequest('GET',endpoint).then(renderResponse());
+			}
+
+	
+	
+
+
+			
